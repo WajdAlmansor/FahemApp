fb48f86860be6df8e3013abb9287b4e76753053c
WajdButterfly <wajd2828@gmail.com>
2024-12-15 17:11:59 +0300

    Initial Commit

---
+++ FahemApp/FahemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FahemAppApp.swift
+//  FahemApp
+//
+//  Created by Wajd on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FahemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
